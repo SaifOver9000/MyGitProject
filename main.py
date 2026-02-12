333c2ce8c44ddf15c0c520d81bdfccf013bb3c15
number1rhymefan <0312089b@acadiau.ca>
2026-02-12 09:41:31 -0400

    Update main.py
    Added function calls

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 Here we will run the functions created in CustonFunctions.py
 We wil also test the thing here
 """
+import CustomFunctions.py
+
 costs = []
 weights = []
 names = []
@@ -13,3 +15,13 @@ while done != "y" or done != "Y":
     costs.append(float(input("What is the cost of the item in dollars?: ")))
     weights.append(float(input("What is the weight of the item in grams?: ")))
     done = input("Are you done? (Y/n): ")
+
+TotalCost(costs)
+TotalWeight(weights)
+OverallCostPer100Grams(costs,weights)
+MeanCost(costs)
+MedianCost(costs)
+MinCost(costs)
+MaxCost(costs)
+MostExpensive(costs,names)
+MostExpensivePerGram(costs,names,weights)
